3d51a654869c377dd939f19871abac885f1d6c2e
dllagl <dllagl@gmx.com>
2021-10-07 21:02:24 +0200

    init

---
+++ main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <boost/array.hpp>
+#include <boost/numeric/odeint.hpp>
+
+
+using namespace std;
+using namespace boost::numeric::odeint;
+
+typedef const double cdouble;
+typedef const int cint;
+
+
+
+int main()
+{
+
+
+    return 0;
+}
