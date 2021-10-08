07c968c5a91f056a1be313334fa5f75cc75ec5a5
dllagl <dllagl@gmx.com>
2021-10-08 11:58:06 +0200

    testing

--- main.cpp
+++ main.cpp
@@ -22,9 +22,9 @@ typedef boost::array<double,4> state_type;
 
 
 // main parameters
-cdouble doping_ratio_sample = 1.;
-cdouble quality_factor      = 2000.;
-cdouble pump_power          = 10. * 1.e7;
+cdouble doping         = 1.0;
+cdouble quality_factor = 2000.0;
+cdouble pump_power     = 10.0 * 1.e7;
 
 const string output_path = "test.txt";
 ofstream ofile(output_path.c_str(), ios::app);
@@ -46,8 +46,8 @@ cdouble ksi = 0.9;
 
 
 // parameters that depends on the sample
-cdouble mol_density = 1.15e-3;
-cdouble dye_density = (doping_ratio_sample * mol_density * AVOGRADRO) / molar_mass;
+cdouble mol_density    = 1.15e-3;
+cdouble dye_density    = (doping * mol_density * AVOGRADRO) / molar_mass;
 cdouble sigma_abs_pump = 8.5e-21;
 cdouble neff           = 1.7;
 
@@ -61,12 +61,21 @@ cdouble sigma_ta = 0.0;
 
 
 // parameters of the laser device
-cdouble gama     = 0.5;
+cdouble gama      = 0.5;
 cdouble nu        = SPEED_LIGHT / lambda_fluo;
-cdouble tau_cav   = quality_factor / (2 * PI * nu);
+cdouble tau_cav   = quality_factor / (2.0 * PI * nu);
 cdouble pump_area = 4.5e-9;
 cdouble spont     = PLANCK*SPEED_LIGHT*SPEED_LIGHT 
                     / (lambda_fluo*pow(pump_area,3./2.));
+
+
+
+
+
+
+
+
+
 
 
 
@@ -83,11 +92,11 @@ void ode_sys(const state_type &x, state_type &dxdt, double t)
 
     // S0 
     dxdt[0] = (
-        (-sigma_abs_pump * lambda_abs * pump_power *x[0]/ HC)
+        ( - sigma_abs_pump * lambda_abs * pump_power * x[0]/ HC )
         + ( x[1] / tau_f )
         + ( x[2] / tau_t )
-        + ( ksta + x[1] * x[2] )
-        + ( 1. * ktta * x[2] * x[2] )
+        + ( ksta * x[1] * x[2] )
+        + ( 1.0 * ktta * x[2] * x[2] )
         + ( kssa * x[1] * x[1] )
         + ( x[1] * sigma_em * x[3] * lambda_fluo / HC)
     );
@@ -96,11 +105,11 @@ void ode_sys(const state_type &x, state_type &dxdt, double t)
     // S1
     dxdt[1] = (
         ( sigma_abs_pump * lambda_abs * pump_power * x[0] / HC )
-        - ( x[1] * (1/tau_f + kisc) )
+        - ( x[1] * (1.0/tau_f + kisc) )
         - ( ksta * x[1] * x[2] )
         + ( 0.25 * ktta * x[2] * x[2] )
-        - (kssa * x[1] * x[1] * (2-ksi) )
-        - (x[1] * sigma_em * x[3] * lambda_fluo / HC )
+        - ( kssa * x[1] * x[1] * (2-ksi) )
+        - ( x[1] * sigma_em * x[3] * lambda_fluo / HC )
     );
 
 
@@ -108,7 +117,7 @@ void ode_sys(const state_type &x, state_type &dxdt, double t)
     dxdt[2] = (
         ( x[1] * kisc )
         - ( x[2] / tau_t )
-        + ( kssa * x[1] * x[1] * (1-ksi) )
+        + ( kssa * x[1] * x[1] * (1.0-ksi) )
         - ( 1.25 * ktta * x[2] * x[2] )
         - ( sigma_triplet_pump * lambda_abs * pump_power * x[2] / HC )
     );
@@ -116,11 +125,16 @@ void ode_sys(const state_type &x, state_type &dxdt, double t)
 
     // photon density
     dxdt[3] =  (
-          ( (SPEED_LIGHT/neff) * dye_density * gama * sigma_em * x[1] * (x[3] + spont) )
+        ( (SPEED_LIGHT/neff) * dye_density * gama * sigma_em * x[1] * (x[3] + spont) )
         - ( (SPEED_LIGHT/neff) * dye_density * gama * sigma_ta * x[2] * x[3] )
         - ( x[3] / tau_cav )
     );
 }
+
+
+
+
+
 
 
 
@@ -151,10 +165,14 @@ void write_output(const state_type &x, const double t)
 
 
 
+
+
+
 int main()
 {
 
-    state_type x = {1., 0.0, 0.0, 0.0};
-    integrate(ode_sys,x,0.0,5.e-6,1.e-9,write_output);
+    state_type x = {1.0, 0.0, 0.0, 0.0};
+    integrate(ode_sys, x, 0.0, 5.e-6, 1.e-9, write_output);
     return 0;
+
 }
