d181fdf68f99d56f14c00409726b429ae0708dbd
dllagl <dllagl@gmx.com>
2021-10-08 23:27:13 +0200

    add user interactions

---
+++ main.py
@@ -0,0 +1,21 @@
+
+
+
+
+import user_interface as ui
+import simu_populations as spop
+
+
+# terminal message when the program gets executed
+ui.welcome()
+
+
+# ask user which simulations to run 
+user_choice = eval(input('Which simulations ? '))
+if user_choice == 1 : 
+    spop.simu()
+else : 
+    print('Error, please try again !')
+    pass
+
+
