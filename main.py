7fa9ae313031efcf9b322b79f2a379c8e3e3a3b8
dllagl <dllagl@gmx.com>
2021-10-08 22:56:24 +0200

    finish first solving

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
+
+
+
+
 # external libraries 
 import numpy as np 
+from scipy.integrate import odeint
 import os
 
 
@@ -18,6 +23,36 @@ ui.welcome()
 const_molecule = imp.param_molecule()
 const_sample   = imp.param_sample(const_molecule[0])
 const_rates    = imp.param_rates(const_sample[2])
+
+
+# pump parameters importation
+const_pump = imp.param_pump()
+
+
+# time vector importation 
+t = imp.param_time(const_pump[0])
+
+
+# initial value for the populations S0,S1,T1
+init_pop = [1,0,0]
+
+
+# output file to write the data on 
+output_file_path = 'test.out'
+if os.path.exists(output_file_path) : os.remove(output_file_path)
+
+
+
+# ode system integration 
+sol = odeint(sys.sys_equations,init_pop,t,
+        args=(const_pump[1],const_sample,const_molecule,const_rates)
+        )
+
+ofile = open(output_file_path,'a+')
+for ii in range(len(sol)) :
+    ofile.write('%.3e\t%.3e\t%.3e\n'
+    % (sol[ii,0],sol[ii,1],sol[ii,2]))
+ofile.close()
 
 
 
