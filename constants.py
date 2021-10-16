1a51603439ed738b1fe6bf3fa9b093d4fa9c6035
dllagl <dllagl@gmx.com>
2021-10-17 00:22:03 +0200

    added comment

--- constants.py
+++ constants.py
@@ -2,9 +2,9 @@
 
 # general physical constants used in the differents files
 
-C_PI    = 3.14157
-C_C     = 299792458.0  
-C_ZETA  = 0.25
-C_AVO   = 6.0221407e23
-C_H     = 6.62607015e-34
+C_PI    = 3.14157         # PI
+C_C     = 299792458.0     # speed of light (m.s-1)
+C_ZETA  = 0.25            # spin statistic singlet creation
+C_AVO   = 6.0221407e23    # Avogadro's contants (mol-1)
+C_H     = 6.62607015e-34  # Planck's constants (J.s-1)
 C_HC    = C_H * C_C 
